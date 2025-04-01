018e376d3876d8c14a82c4c98569c992479faa48
Slava <136799263+Zdssla@users.noreply.github.com>
2025-04-01 21:48:34 +0000

    Добавить настройки VSCode, обновить маршруты викторины, изменить шаблоны курсов и добавить новый курс

--- website/quiz/urls.py
+++ website/quiz/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import quiz_view, quiz_list_view
 
 urlpatterns = [
-    path('', quiz_list_view, name='courses'),  # Новый маршрут для списка курсов
-    path('<int:quiz_id>/', quiz_view, name='quiz'),
+    path('', quiz_list_view, name='courses'), 
+    path('', quiz_list_view, name='first'), 
+    #path('<int:quiz_id>/', quiz_view, name='quiz'),
 ]
